cf96442324fd067fc787d872e7b2c51056ebfa65
filipvukovic <fvukovic123@gmail.com>
2024-03-22 23:35:24 +0100

    ECB with padding implemented!

--- src/main.c
+++ src/main.c
@@ -5,90 +5,6 @@ int main(int argc, char *argv[])
 {
     enum key_size size = SIZE_16;
 
-    int expandedKeySize = 176;
-
-    unsigned char expandedKey[expandedKeySize];
-
-    unsigned char key[] = {"myfirstaescipher"};
-
-    unsigned char plaintext[] = {"filip123321abcde"};
-
-    unsigned char ciphertext[16];
-
-    unsigned char decryptedtext[16];
-
-    printf("\nCipher Key (HEX format):\n");
-    printBufferHex(key, 16);
-    
-
-    key_expansion(expandedKey, key, size);
-
-    printf("\nExpanded Key (HEX format):\n");
-
-    for (int i = 0; i < expandedKeySize; i++)
-    {
-        printf("%2.2x%c", expandedKey[i], ((i + 1) % 16) ? ' ' : '\n');
-    }
-
-    printf("\nPlaintext (HEX format):\n");
-    printBufferHex(plaintext, 16);
-
-    aes_encrypt_block(plaintext, ciphertext, key, size);
-
-    printf("\nCiphertext (HEX format):\n");
-    printBufferHex(ciphertext, 16);
-
-    aes_decrypt_block(ciphertext, decryptedtext, key, size);
-
-    printf("\nDecrypted text (HEX format):\n");
-    printBufferHex(decryptedtext, 16);
-
-
-    // unsigned char key1[] = {"myfirstaescipher"};
-
-    // unsigned char plaintext1[] = {"abcdef1234567890filip123321abcde"};
-
-    // unsigned char ciphertext1[32];
-
-    // unsigned char decryptedtext1[32];
-
-    // printf("\nPlaintext (HEX format):\n");
-    // printBufferHex(plaintext1, 32);
-
-    // aes_ecb_encrypt(plaintext1, 32, key1, SIZE_16, ciphertext1, NULL);
-
-    // printf("\nCiphertext (HEX format):\n");
-    // printBufferHex(ciphertext1, 32);
-
-    // aes_ecb_decrypt(ciphertext1, 32, key1, SIZE_16, decryptedtext1, NULL);
-
-    // printf("\nDecrypted text (HEX format):\n");
-    // printBufferHex(decryptedtext1, 32);
-
-
-    // unsigned char key2[] = {"myfirstaescipher"};
-    // unsigned char iv[] = {"veryfirstinitvec"};
-
-    // unsigned char plaintext2[] = {"abcdef1234567890filip123321abcdeabcdef1234567890filip123321abcde"};
-
-    // unsigned char ciphertext2[64];
-
-    // unsigned char decryptedtext2[64];
-
-    // printf("\nPlaintext (HEX format):\n");
-    // printBufferHex(plaintext2, 64);
-
-    // aes_cbc_encrypt(plaintext2, 64, key2, SIZE_16, iv, ciphertext2, NULL);
-
-    // printf("\nCiphertext (HEX format):\n");
-    // printBufferHex(ciphertext2, 64);
-
-    // aes_cbc_decrypt(ciphertext2, 64, key2, SIZE_16, iv, decryptedtext2, NULL);
-
-    // printf("\nDecrypted text (HEX format):\n");
-    // printBufferHex(decryptedtext2, 64);
-
-
     unsigned char key3[] = {"myfirstaescipher"};
     unsigned char nonce[] = {"veryfirstinitvez"};
 
@@ -96,23 +12,23 @@ int main(int argc, char *argv[])
 
     int outsize;
 
-    unsigned char ciphertext3[67];
+    unsigned char ciphertext3[100];
 
-    unsigned char decryptedtext3[67];
+    unsigned char decryptedtext3[100];
 
     printf("\nPlaintext (HEX format):\n");
     printBufferHex(plaintext3, 67);
 
-    aes_ofb_xxcrypt(plaintext3, 67, key3, SIZE_16, nonce, ciphertext3, &outsize);
+    aes_cbc_encrypt(plaintext3, 67, key3, SIZE_16, nonce, ciphertext3, &outsize);
 
     printf("\nCiphertext (HEX format):\n");
-    printBufferHex(ciphertext3, 67);
+    printBufferHex(ciphertext3, outsize);
 
 
-    aes_ofb_xxcrypt(ciphertext3, 67, key3, SIZE_16, nonce, decryptedtext3, &outsize);
+    aes_cbc_decrypt(ciphertext3, outsize, key3, SIZE_16, nonce, decryptedtext3, &outsize);
 
     printf("\nDecrypted text (HEX format):\n");
-    printBufferHex(decryptedtext3, 67);
+    printBufferHex(decryptedtext3, outsize);
 
     return 0;
 
--- src/modes.c
+++ src/modes.c
@@ -8,48 +8,84 @@ static void xor_block(unsigned char *in_1, unsigned char *in_2, int in_size, cha
     }
 }
 
+static void create_PKCS7_buffer(unsigned char* input_buffer, int input_buffer_size, int bytes_to_append, unsigned char* output_buffer) {
+    int padded_size = input_buffer_size + bytes_to_append;
+    int pad_length = bytes_to_append;
+
+    memcpy(output_buffer, input_buffer, input_buffer_size);
+
+    for (int i = input_buffer_size; i < padded_size; i++) {
+        output_buffer[i] = (unsigned char)pad_length;
+    }
+}
+
 int aes_cbc_encrypt(unsigned char *in, int in_size, unsigned char* key, enum key_size key_size, unsigned char* iv, unsigned char* out, int* out_size){
     int num_of_blocks = in_size / AES_BLOCK_SIZE;
+    int modulo = in_size % AES_BLOCK_SIZE;
+    unsigned char padding_buffer[16];
+
+    create_PKCS7_buffer(in + (in_size - modulo), modulo, AES_BLOCK_SIZE - modulo, padding_buffer);
     
     unsigned char tmp [16];
+    
     xor_block(in, iv, AES_BLOCK_SIZE, tmp);
     aes_encrypt_block(tmp, out, key, key_size);
-    
+    *out_size = AES_BLOCK_SIZE;
     for(int i = 0; i < num_of_blocks - 1; i++){
         xor_block(out + (i * AES_BLOCK_SIZE), in + ((i + 1) * AES_BLOCK_SIZE), AES_BLOCK_SIZE, tmp);
         aes_encrypt_block(tmp, out + ((i + 1) * AES_BLOCK_SIZE), key, key_size);
+        *out_size += AES_BLOCK_SIZE;
     }
+    xor_block(out + num_of_blocks * AES_BLOCK_SIZE, padding_buffer, AES_BLOCK_SIZE, tmp);
+    aes_encrypt_block(tmp, out + num_of_blocks * AES_BLOCK_SIZE, key, key_size);
+    *out_size += AES_BLOCK_SIZE;
 }
 
 
 int aes_cbc_decrypt(unsigned char *in, int in_size, unsigned char* key, enum key_size key_size, unsigned char* iv, unsigned char* out, int* out_size){
     int num_of_blocks = in_size / AES_BLOCK_SIZE;
-    
+    int modulo = in_size % AES_BLOCK_SIZE;
+
     unsigned char tmp[16];
+    
     aes_decrypt_block(in, tmp, key, key_size);
     xor_block(tmp, iv, AES_BLOCK_SIZE, out);
+    *out_size = AES_BLOCK_SIZE;
     
     for(int i = 0; i < num_of_blocks - 1; i++){
         aes_decrypt_block(in + ((i + 1) * AES_BLOCK_SIZE), tmp, key, key_size);
         xor_block(tmp, in + (i * AES_BLOCK_SIZE), AES_BLOCK_SIZE, out + ((i + 1) * AES_BLOCK_SIZE));
+        *out_size += AES_BLOCK_SIZE;
     }
-
+    // *out_size -= out[in_size - 1];
 }
 
 int aes_ecb_encrypt(unsigned char* in, int in_size, unsigned char* key, enum key_size key_size, unsigned char* out, int* out_size){
     int num_of_blocks = in_size / AES_BLOCK_SIZE;
+    int modulo = in_size % AES_BLOCK_SIZE;
 
+    unsigned char padding_buffer[16];
+
+    create_PKCS7_buffer(in + (in_size - modulo), modulo, AES_BLOCK_SIZE - modulo, padding_buffer);
+    *out_size = 0;
     for(int i = 0; i < num_of_blocks; i ++){
         aes_encrypt_block(in + (i * AES_BLOCK_SIZE), out + (i * AES_BLOCK_SIZE), key, key_size);
+        *out_size += AES_BLOCK_SIZE;
     }
+    aes_encrypt_block(padding_buffer, out + (num_of_blocks * AES_BLOCK_SIZE), key, key_size);
+    *out_size += AES_BLOCK_SIZE;
 }
 
 int aes_ecb_decrypt(unsigned char* in, int in_size, unsigned char* key, enum key_size key_size, unsigned char* out, int* out_size){
     int num_of_blocks = in_size / AES_BLOCK_SIZE;
+    int modulo = in_size % AES_BLOCK_SIZE;
 
+    *out_size = 0;
     for(int i = 0; i < num_of_blocks; i ++){
         aes_decrypt_block(in + (i * AES_BLOCK_SIZE), out + (i * AES_BLOCK_SIZE), key, key_size);
+        *out_size += AES_BLOCK_SIZE;
     }
+    *out_size -= out[in_size - 1];
 }
 
 static void increment_nonce(unsigned char *nonce) {
@@ -112,7 +148,7 @@ int aes_cfb_encrypt(unsigned char *in, int in_size, unsigned char* key, enum key
     if (modulo > 0){
         aes_encrypt_block(out + (num_of_blocks - 1)*AES_BLOCK_SIZE, tmp, key, key_size);
         xor_block(tmp, in + num_of_blocks*AES_BLOCK_SIZE, modulo, out + num_of_blocks*AES_BLOCK_SIZE);
-        *out_size = modulo;
+        *out_size += modulo;
     }
 }
 
@@ -135,7 +171,7 @@ int aes_cfb_decrypt(unsigned char *in, int in_size, unsigned char* key, enum key
     if (modulo > 0){
         aes_encrypt_block(in + (num_of_blocks - 1)*AES_BLOCK_SIZE, tmp, key, key_size);
         xor_block(tmp, in + num_of_blocks*AES_BLOCK_SIZE, modulo, out + num_of_blocks*AES_BLOCK_SIZE);
-        *out_size = modulo;
+        *out_size += modulo;
     }
 }
 
@@ -160,6 +196,6 @@ int aes_ofb_xxcrypt(unsigned char *in, int in_size, unsigned char* key, enum key
     if (modulo > 0){
         aes_encrypt_block(tmp, tmp, key, key_size);
         xor_block(in + num_of_blocks*AES_BLOCK_SIZE, tmp, modulo, out + num_of_blocks*AES_BLOCK_SIZE);
-        *out_size = modulo;
+        *out_size += modulo;
     }
 }
